385af2d7d492cc80d5fad98ad14f0dde8031bb6e
Andreas Nicolaj Tietgen <andreotietgen@hotmail.com>
2021-10-22 17:31:34 +0200

    Move item to seperate file

--- GildedRose.Console/Program.cs
+++ GildedRose.Console/Program.cs
@@ -125,13 +125,6 @@ namespace GildedRose.Console
 
     }
 
-    public class Item
-    {
-        public string Name { get; set; }
-
-        public int SellIn { get; set; }
-
-        public int Quality { get; set; }
-    }
+    
 
 }
