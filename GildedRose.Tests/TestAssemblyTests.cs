db0f8cf70466d5d03fc6ee39305cff6410eb4fa3
Arne4545 <andreotietgen@hotmail.com>
2021-10-23 16:10:03 +0200

    Feature/test (#1)
    * Add gitignore
    
    Co-authored-by: Joachim Borup <JoachimBorup@users.noreply.github.com>
    
    * Implement initial test
    
    Co-authored-by: Joachim Borup <JoachimBorup@users.noreply.github.com>
    Co-authored-by: YoHype <YoHype@users.noreply.github.com>
    
    * Add missing pieces
    
    * Add more missing pieces
    
    * Exclude Main method in code coverage report
    
    * Merge changes
    
    Co-authored-by: Joachim Borup <JoachimBorup@users.noreply.github.com>
    Co-authored-by: YoHype <YoHype@users.noreply.github.com>
    Co-authored-by: aljb <aljb@itu.dk>

--- GildedRose.Tests/TestAssemblyTests.cs
+++ GildedRose.Tests/TestAssemblyTests.cs
@@ -1,13 +1,128 @@
-﻿using Xunit;
+﻿using GildedRose;
+using System.Collections.Generic;
+using Xunit;
 
 namespace GildedRose.Tests
 {
     public class TestAssemblyTests
     {
-        [Fact]
-        public void TestTheTruth()
+        public static TheoryData<Item, int> QualityUpdateTestData => new TheoryData<Item, int> {
+            { new Item { Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20 }, 19 },
+            { new Item { Name = "Aged Brie", SellIn = 2, Quality = 0 }, 1 },
+            { new Item { Name = "Elixir of the Mongoose", SellIn = 5, Quality = 7 }, 6 },
+            { new Item { Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 80 }, 80 },
+            { new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 15, Quality = 20 }, 21 },
+            { new Item { Name = "Conjured Mana Cake", SellIn = 3, Quality = 6 }, 5 }
+        };
+
+        private Item _vest;
+        private Item _brie;
+        private Item _elixir;
+        private Item _sulfuras;
+        private Item _backstagePass;
+        private Item _conjured;
+
+        public TestAssemblyTests()
         {
-            Assert.True(true);
+            _vest = new Item { Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20 };
+            _brie = new Item { Name = "Aged Brie", SellIn = 2, Quality = 0 };
+            _elixir = new Item { Name = "Elixir of the Mongoose", SellIn = 5, Quality = 7 };
+            _sulfuras = new Item { Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 80 };
+            _backstagePass = new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 15, Quality = 20 };
+            _conjured = new Item { Name = "Conjured Mana Cake", SellIn = 3, Quality = 6 };
+        }
+
+        [Theory]
+        [MemberData(nameof(QualityUpdateTestData))]
+        public void UpdateQuality_QualityIsCorrect(Item item, int expectedQuality)
+        {
+            Program p = new Program();
+            p.Items = new List<Item>() { item };
+            p.UpdateQuality();
+
+            Assert.Equal(expectedQuality, item.Quality);
+        }
+
+        [Fact]
+        public void UpdateQuality_DoesNotSetNegativeQuality()
+        {
+            Program p = new Program();
+            p.Items = new List<Item>() { _vest, _elixir };
+            
+            for (int i = 0; i < 20; i++)
+            {
+                p.UpdateQuality();
+            }
+
+            foreach (var item in p.Items)
+            {
+                Assert.Equal(0, item.Quality);
+            }
+        }
+
+        [Fact]
+        public void UpdateQuality_DoesNotSetQualityAbove50()
+        {
+            Program p = new Program();
+            p.Items = new List<Item>() { _brie, _backstagePass };
+
+            for (int i = 0; i < 50; i++)
+            {
+                p.UpdateQuality();
+            }
+
+            foreach (var item in p.Items)
+            {
+                // Assert.Equal();
+            }
+        }
+
+        [Fact]
+        public void UpdateQuality_IncreasesBrieQuality()
+        {
+            Program p = new Program();
+            p.Items = new List<Item>() { _brie };
+
+            int prevQuality = _brie.Quality;
+
+            // Brie increases in quality the older it gets
+            while (_brie.SellIn > 0)
+            {
+                p.UpdateQuality();
+                Assert.Equal(prevQuality + 1, _brie.Quality);
+                prevQuality = _brie.Quality;
+            }
+
+            // Once the sell by date has passed, the quality of brie increases twice as fast
+            for (int i = 0; i < 5; i++)
+            {
+                p.UpdateQuality();
+                Assert.Equal(prevQuality + 2, _brie.Quality);
+                prevQuality = _brie.Quality;
+            }
+        }
+
+        [Fact]
+        public void Sulfuras_Never_Has_To_Be_Sold_Or_Decrease_In_Value()
+        {
+            //Given
+            Program p = new Program();
+            _sulfuras.SellIn = 10;
+            _sulfuras.Quality = 80;
+            p.Items = new List<Item>() { _sulfuras };
+
+            //When
+            for (int i = 0; i < 8; i++)
+            {
+                p.UpdateQuality();
+            }
+            
+            //Then
+            foreach (var item in p.Items)
+            {
+                Assert.Equal(10, item.SellIn);
+                Assert.Equal(80, item.Quality);
+            }
         }
     }
 }
