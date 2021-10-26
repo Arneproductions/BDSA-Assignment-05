8624076f86c4733dc492cb48257d95acf6ffadd2
Andreas Nicolaj Tietgen <andreotietgen@hotmail.com>
2021-10-26 14:35:57 +0200

    Change quality rule to use sellin

--- GildedRose/QualityAssessments/IncreaseAssessment.cs
+++ GildedRose/QualityAssessments/IncreaseAssessment.cs
@@ -6,17 +6,17 @@ namespace GildedRose.QualityAssessments
 {
     public class IncreaseAssessment : IQualityAssessment
     {
-        private readonly IEnumerable<(int quality, int increaseAmount)> _increaseAmountRules;
+        private readonly IEnumerable<(int sellIn, int increaseAmount)> _increaseAmountRules;
 
-        public IncreaseAssessment(params (int quality, int increaseAmount)[] increaseAmountRules)
+        public IncreaseAssessment(params (int sellIn, int increaseAmount)[] increaseAmountRules)
         {
-            _increaseAmountRules = increaseAmountRules.OrderBy(x => x.quality);
+            _increaseAmountRules = increaseAmountRules.OrderBy(x => x.sellIn);
         }
 
         public int AssessQuality(in int currentQuality, in int currentSellIn)
         {
             foreach (var rule in _increaseAmountRules)
-                if(rule.quality > currentQuality)
+                if(rule.sellIn >= currentSellIn)
                     return currentQuality + rule.increaseAmount;
 
             return currentQuality;
