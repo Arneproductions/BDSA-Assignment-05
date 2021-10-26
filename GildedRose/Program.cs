73af2ec317c8d881ad36baa8a5fa3e99ff8cab55
Andreas Nicolaj Tietgen <andreotietgen@hotmail.com>
2021-10-26 15:04:56 +0200

    Add assessment rules to program

--- GildedRose/Program.cs
+++ GildedRose/Program.cs
@@ -18,9 +18,9 @@ namespace GildedRose
             {
                 Items = new List<Item>
                 {
-                    new Item { Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20, QualityAssesser = new MinMaxAssessment(0, 50) },
-                    new Item { Name = "Aged Brie", SellIn = 2, Quality = 0, QualityAssesser = new MinMaxAssessment(0, 50) },
-                    new Item { Name = "Elixir of the Mongoose", SellIn = 5, Quality = 7, QualityAssesser = new MinMaxAssessment(0, 50) },
+                    new Item { Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20, QualityAssesser = new MinMaxAssessment(0, 50, new IncreaseAssessment((int.MaxValue, -1), (0, -2))) },
+                    new Item { Name = "Aged Brie", SellIn = 2, Quality = 0, QualityAssesser = new MinMaxAssessment(0, 50, new IncreaseAssessment((int.MaxValue, 1), (0, 2))) },
+                    new Item { Name = "Elixir of the Mongoose", SellIn = 5, Quality = 7, QualityAssesser = new MinMaxAssessment(0, 50, new IncreaseAssessment((int.MaxValue, -1), (0, -2))) },
                     new Item { Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 80, QualityAssesser = new MinMaxAssessment(80, 80) },
                     new Item { Name = "Sulfuras, Hand of Ragnaros", SellIn = -1, Quality = 80, QualityAssesser = new MinMaxAssessment(80, 80) },
                     new Item
@@ -28,24 +28,24 @@ namespace GildedRose
                         Name = "Backstage passes to a TAFKAL80ETC concert",
                         SellIn = 15,
                         Quality = 20,
-                        QualityAssesser = new MinMaxAssessment(0, 50, new IncreaseAssessment())
+                        QualityAssesser = new MinMaxAssessment(0, 50, new IncreaseAssessment((50, 1), (5, 3), (10, 2), (0, int.MinValue)))
                     },
                     new Item
                     {
                         Name = "Backstage passes to a TAFKAL80ETC concert",
                         SellIn = 10,
                         Quality = 49,
-                        QualityAssesser = new MinMaxAssessment(0, 50, new IncreaseAssessment())
+                        QualityAssesser = new MinMaxAssessment(0, 50, new IncreaseAssessment((50, 1), (5, 3), (10, 2), (0, int.MinValue)))
                     },
                     new Item
                     {
                         Name = "Backstage passes to a TAFKAL80ETC concert",
                         SellIn = 5,
                         Quality = 49,
-                        QualityAssesser = new MinMaxAssessment(0, 50, new IncreaseAssessment())
+                        QualityAssesser = new MinMaxAssessment(0, 50, new IncreaseAssessment((50, 1), (5, 3), (10, 2), (0, int.MinValue)))
                     },
                     // this conjured item does not work properly yet
-                    new Item { Name = "Conjured Mana Cake", SellIn = 3, Quality = 6, QualityAssesser = new MinMaxAssessment(0, 50) }
+                    new Item { Name = "Conjured Mana Cake", SellIn = 3, Quality = 6, QualityAssesser = new MinMaxAssessment(0, 50, new IncreaseAssessment((int.MaxValue, -2), (0, -4))) }
                 }
             };
 
