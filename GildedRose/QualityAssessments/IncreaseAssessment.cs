bcfd93c2084abd15719b5562a391fafb0e8e40b9
Andreas Nicolaj Tietgen <andreotietgen@hotmail.com>
2021-10-28 07:35:44 +0200

    Add wrapper pattern

--- GildedRose/QualityAssessments/IncreaseAssessment.cs
+++ GildedRose/QualityAssessments/IncreaseAssessment.cs
@@ -6,20 +6,30 @@ namespace GildedRose.QualityAssessments
 {
     public class IncreaseAssessment : IQualityAssessment
     {
+        private readonly IQualityAssessment _qualityAssessment;
         private readonly IEnumerable<(int sellIn, int increaseAmount)> _increaseAmountRules;
 
-        public IncreaseAssessment(params (int sellIn, int increaseAmount)[] increaseAmountRules)
+        public IncreaseAssessment(IQualityAssessment qualityAssessment, params (int sellIn, int increaseAmount)[] increaseAmountRules)
         {
+            _qualityAssessment = qualityAssessment;
             _increaseAmountRules = increaseAmountRules.OrderBy(x => x.sellIn);
+        }
+
+        public IncreaseAssessment(params (int sellIn, int increaseAmount)[] increaseAmountRules)
+            : this(null, increaseAmountRules)
+        {
+            
         }
 
         public int AssessQuality(in int currentQuality, in int currentSellIn)
         {
+            int quality = _qualityAssessment != null ? _qualityAssessment.AssessQuality(currentQuality, currentSellIn) : currentQuality;
+
             foreach (var rule in _increaseAmountRules)
                 if(rule.sellIn >= currentSellIn)
-                    return currentQuality + rule.increaseAmount;
+                    return quality + rule.increaseAmount;
 
-            return currentQuality;
+            return quality;
         }
     }
 }
